e7b17f327ca55c84c387ca87ecd1d344c45be11c
landofbol <sam@theoryfarm.com>
2026-02-09 00:00:52 -0500

    Updated Dockerfile and firewall for langfuse compatibility

--- .devcontainer/init-firewall.sh
+++ .devcontainer/init-firewall.sh
@@ -78,6 +78,8 @@ for domain in \
     "objects.githubusercontent.com" \
     "api.cloudflare.com" \
     "update.code.visualstudio.com" \
+    "storage.googleapis.com" \
+    "pypi.python.org" \
     "adventure-alerts-api.sam-ed4.workers.dev"; do
     echo "Resolving $domain..."
     ips=$(dig +noall +answer A "$domain" | awk '$4 == "A" {print $5}')
